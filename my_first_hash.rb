b2ff4e86f313e8e72d876bf1b36eacce3f9b2fad
Dan Ste. Marie Jr <dste.mariejr@gmail.com>
2020-04-19 03:59:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,8 +9,8 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
   shipping_manifest["oil paintings"]
@@ -18,13 +18,13 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
 
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
-  
+
   shipping_manifest
 end
